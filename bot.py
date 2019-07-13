3897d3e5dbe4b2893be28078d615364bbaccfdba
Mark Wingerd <mawingerd@gmail.com>
2019-07-12 17:02:41 -0700

    Add server shutdown function

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from tools import os_utils, permissions
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -14,12 +15,14 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.command()
 async def server_status(ctx):
     if os_utils.get_process():
         await ctx.send('Server is currently online')
     else:
         await ctx.send('Server is currently offline')
+
 
 @bot.command()
 async def server_start(ctx):
@@ -31,13 +34,30 @@ async def server_start(ctx):
         return
 
     os_utils.run_script('server_start.sh')
-    await ctx.send('Starting the server'.format(ctx.author))
+    await ctx.send('Starting the server')
     await asyncio.sleep(3)
     if os_utils.get_process():
         await ctx.send('Server is online')
     else:
         await ctx.send('Server failed to start')
 
+
+@bot.command()
+async def server_shutdown(ctx):
+    if not permissions.authorized(ctx):
+        await ctx.send('You are not authorized to use this command'.format(ctx.author))
+        return
+    if os_utils.get_process():
+        await ctx.send('Server is already online')
+        return
+
+    os_utils.run_script('server_shutdown.sh')
+    await ctx.send('Shutting down the server')
+    await asyncio.sleep(3)
+    if os_utils.get_process():
+        await ctx.send('Server is online')
+    else:
+        await ctx.send('Server is offline')
 
 
 @bot.command()
@@ -46,5 +66,6 @@ async def test(ctx):
         await ctx.send('You are not authorized to use this command'.format(ctx.author))
         return
     await ctx.send('I heard you! {0}'.format(ctx.author))
+
 
 bot.run(os_utils.get_env('DISCORD_TOKEN'))
