184a27614baa933b76c7abaa9403c4f10cc0332d
Mark Wingerd <mawingerd@gmail.com>
2019-07-19 15:49:28 -0700

    Add map remove and add

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 
 from tools import os_utils, permissions
 
+GAME_INI = '/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini'
+
 
 bot = commands.Bot(command_prefix='!')
 
@@ -82,23 +84,43 @@ async def server_restart(ctx):
 
 @bot.command()
 async def map_list(ctx):
-    lines = os_utils.get_file('/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini')
+    lines = os_utils.get_file(GAME_INI)
     # Keep all lines that start with MapRotation
     lines = [line for line in lines if line.startswith('MapRotation')]
     # TODO: Add html fetching and parsing for beautiful presentation
-    msg = _create_map_list(lines)
+    msg = _create_maps_string(lines)
     await ctx.send(msg)
 
 
 @bot.command()
-async def test(ctx):
+async def map_remove(ctx, map_):
     if not permissions.authorized(ctx):
         await ctx.send('You are not authorized to use this command'.format(ctx.author))
         return
+
+    lines = os_utils.get_file(GAME_INI)
+    lines = (l for l in lines if not (l.startswith('MapRotation') and map_ in l))
+    os_utils.save_file(GAME_INI, lines)
+
+
+@bot.command()
+async def map_add(ctx, map_, mode='SND'):
+    if not permissions.authorized(ctx):
+        await ctx.send('You are not authorized to use this command'.format(ctx.author))
+        return
+
+    lines = os_utils.get_file(GAME_INI)
+    lines.append('MapRotation=(MapId="{map_}", GameMode="{mode}")'.format(map_=map_, mode=mode))
+    os_utils.save_file(GAME_INI, lines)
+
+@bot.command()
+async def test(ctx):
+    print(ctx.kwargs)
+    print(dir(ctx.kwargs))
     await ctx.send('I heard you! {0}'.format(ctx.author))
 
 
-def _create_map_list(lines):
+def _create_maps_string(lines):
     url = 'https://steamcommunity.com/sharedfiles/filedetails/?id='
     msg = []
     for line in lines:
